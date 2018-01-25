f8be8767a0bfc0b9609ce35fe51e6eb8cb613fb4
Nick <sirninjamonkey@gmail.com>
2018-01-25 13:44:55 -0600

    removed code for init function; renamed all variables and functions
    all DLLs will be expected to use DLLMain instead of an init function

--- main.cpp
+++ main.cpp
@@ -5,74 +5,48 @@
 #include <fstream>
 #include <vector>
 
-#define INIT_FUNCTION "tl2i_init"
-
 using namespace std;
 
 namespace TL2Injector 
 {
-	bool start_game(PROCESS_INFORMATION*); // launches the game, returns true if successful
-	bool inject_dll(HANDLE*, string );
-	bool get_dll_list(vector<string>*);
+	bool StartGame(PROCESS_INFORMATION*); // launches the game, returns true if successful
+	bool InjectDll(HANDLE*, string );
+	bool GetDllList(vector<string>*);
 }
 
-// process_info is a pointer used for output
-bool TL2Injector::start_game(PROCESS_INFORMATION * process_info)
+// processInfo is a pointer used for output
+bool TL2Injector::StartGame(PROCESS_INFORMATION * processInfo)
 {
-	bool success = CreateProcess("Torchlight2.exe", NULL, NULL, NULL, TRUE, 0, NULL, NULL, NULL, process_info);
+	bool success = CreateProcess("Torchlight2.exe", NULL, NULL, NULL, TRUE, 0, NULL, NULL, NULL, processInfo);
 	return success;
 }
 
-bool inject_dll(HANDLE * target_process, string dll_name)
+bool InjectDll(HANDLE * targetProcess, string dllName)
 {
-	const char * dll_name_cstr = dll_name.c_str();
-	int dll_name_len = dll_name.length()+1;
-	DWORD lib_address = 0;
-	LPTHREAD_START_ROUTINE load_dll_address = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("Kernel32"), "LoadLibraryA");
-	LPTHREAD_START_ROUTINE free_dll_address = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("Kernel32"), "FreeLibrary");
+	const char * dllNameCstr = dllName.c_str();
+	int dllNameLen = dllName.length()+1;
+	LPTHREAD_START_ROUTINE loadDllAddress = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("Kernel32"), "LoadLibraryA");
+	
 	// Store the DLL name in the process's memory so we can load it
-	void * lib_name_memory = VirtualAllocEx(target_process, NULL, dll_name_len, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	//printf("Address: %p - size: %d \r\n", lib_name_memory, dll_name_len);
-	WriteProcessMemory(target_process, lib_name_memory, dll_name_cstr, dll_name_len, NULL);
-	HANDLE injection_thread = CreateRemoteThread(target_process, NULL, 0, load_dll_address, lib_name_memory, 0, NULL);
-	WaitForSingleObject(injection_thread, INFINITE);
-	GetExitCodeThread(injection_thread, &lib_address );
-	//printf("DLL Address %p \r\n", (void*)lib_address);
-	VirtualFreeEx(target_process, lib_name_memory, dll_name_len, MEM_RELEASE);
-	CloseHandle(injection_thread);
+	void * libNameMemory = VirtualAllocEx(targetProcess, NULL, dllNameLen, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	WriteProcessMemory(targetProcess, libNameMemory, dllNameCstr, dllNameLen, NULL);
+	HANDLE injectionThread = CreateRemoteThread(targetProcess, NULL, 0, loadDllAddress, libNameMemory, 0, NULL);
+	WaitForSingleObject(injectionThread, INFINITE);
+	VirtualFreeEx(targetProcess, libNameMemory, dllNameLen, MEM_RELEASE);
+	CloseHandle(injectionThread);
 	
-	// Call init function
-	// we must load the DLL first to find the function
-	HMODULE loaded_lib = LoadLibrary(dll_name_cstr);
-	// make sure the library has the init function - if not don't inject it
-	if (GetProcAddress(loaded_lib, INIT_FUNCTION) == NULL)
-	{
-		FreeLibrary(loaded_lib);
-		return false;
-	}
-	
-	DWORD init_offset = (DWORD)(GetProcAddress(loaded_lib, INIT_FUNCTION)) - (DWORD)(loaded_lib);
-	FreeLibrary(loaded_lib); // we've got the address so we dont need this anymore
-	LPTHREAD_START_ROUTINE init_address = (LPTHREAD_START_ROUTINE)(init_offset + lib_address);
-	HANDLE init_thread = CreateRemoteThread(target_process, NULL, 0, init_address, NULL, 0, NULL);
-	WaitForSingleObject(init_thread, INFINITE);
-	CloseHandle(init_thread);
-	
-	//DLL injected - unload it now
-	HANDLE unload_thread = CreateRemoteThread(target_process, NULL, 0, free_dll_address, (void*)lib_address, 0, NULL);
-	WaitForSingleObject(unload_thread, INFINITE);
 	return true;
 }
 
-bool TL2Injector::get_dll_list(vector<string> * filenames)
+bool TL2Injector::GetDllList(vector<string> * filenames)
 {
 	// iterate through each file in the subdirectory "TL2I" and save the filenames to a vector
-	WIN32_FIND_DATA current_file_data;
-	HANDLE file_search_handle = FindFirstFile(".\\TL2I\\*.dll", &current_file_data);
-	while (file_search_handle != INVALID_HANDLE_VALUE)
+	WIN32_FIND_DATA currentFileData;
+	HANDLE fileSearchHandle = FindFirstFile(".\\TL2I\\*.dll", &currentFileData);
+	while (fileSearchHandle != INVALID_HANDLE_VALUE)
 	{
-		filenames->push_back( string(current_file_data.cFileName) );
-		FindNextFile(file_search_handle, &current_file_data);
+		filenames->push_back( string(currentFileData.cFileName) );
+		FindNextFile(fileSearchHandle, &currentFileData);
 	}
 	if (filenames->size() == 0)
 	{
@@ -83,27 +57,27 @@ bool TL2Injector::get_dll_list(vector<string> * filenames)
 
 int main(int argc, char * argv[])
 {
-	PROCESS_INFORMATION process_info;
-	if ( !TL2Injector::start_game(&process_info) ) // attempt to start the game
+	PROCESS_INFORMATION processInfo;
+	if ( !TL2Injector::StartGame(&processInfo) ) // attempt to start the game
 	{
 		printf("Unable to start game.");
 		return -1;
 	}
-	HANDLE * process_handle = &(process_info.hProcess); 
+	HANDLE * processHandle = &(processInfo.hProcess); 
 	
 	// now get a list of every DLL in the subdirectory TL2I
-	vector<string> dll_filenames;
-	if ( !TL2Injector::get_dll_list(&dll_filenames) )
+	vector<string> dllFilenames;
+	if ( !TL2Injector::GetDllList(&dllFilenames) )
 	{
 		printf("No DLLs loaded.");
 	}
 	
 	// Once we have all the DLL names, iterate through and inject them
-	for (unsigned int i=0; i<dll_filenames.size(); i++)
+	for (unsigned int i=0; i<dllFilenames.size(); i++)
 	{
-		if ( !inject_dll(process_handle, dll_filenames[i]) )
+		if ( !InjectDll(processHandle, dllFilenames[i]) )
 		{
-			printf("Injection failed for file %s", dll_filenames[i].c_str());
+			printf("Injection failed for file %s", dllFilenames[i].c_str());
 		}
 	}
 	
